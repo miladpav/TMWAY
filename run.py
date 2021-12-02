dee56829d77430fcc2e5930291faca6698770e04
MilaD Allahverdi <milad.alhvrd@gmail.com>
2021-12-02 20:05:46 +0330

    remove useless config from run

--- run.py
+++ run.py
@@ -4,13 +4,13 @@ import socket
 
 ## --------------------------------------------------------------------- ##
 
-host = app.config['HTTP_HOST']
 port = app.config['HTTP_PORT']
 protocol = app.config['HTTP_PROTOCOL']
+prefix = app.config['TMWAY_PREFIX']
 
 hostname = socket.gethostname()
 server_ip = socket.gethostbyname(hostname)
 
 if __name__ == '__main__':
-    print(f"TMWAY is available on {protocol}://{server_ip}:{port}\n")
+    print(f"TMWAY is available on {protocol}://{server_ip}:{port}/{prefix}\n")
     WSGIServer(('0.0.0.0', port), app).serve_forever()
