e6d15ffd2af18550c81c9bdf741ee800219edca6
MilaD Allahverdi <milad.alhvrd@gmail.com>
2021-12-02 14:29:39 +0330

    remove jasonify lib

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from gevent.pywsgi import WSGIServer
 import json, re
 from os.path import exists
