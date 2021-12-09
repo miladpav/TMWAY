9a0ce736c026deefc46c7632958b894c75041027
MilaD Allahverdi <milad.alhvrd@gmail.com>
2021-12-09 14:17:36 +0330

    yaml reader is done

--- app/pattern.py
+++ app/pattern.py
@@ -9,9 +9,13 @@ def pattern_reader(ymlFile):
     if exists(ymlFile):
         with open(ymlFile, 'r') as pattern_file:
             pattern_file_buffer = yaml.load(pattern_file, Loader=yaml.FullLoader)
-            if str(pattern_file_buffer.keys()[0]) == "patterns":
-                for pattern in pattern_file_buffer.values():
+            for pattern_title, patterns in pattern_file_buffer.items():
+            # if list(pattern_file_buffer.keys())[0] == "patterns":
+                #print(pattern_title, ": ", patterns[0])
+                #pattern_list.append(pattern for pattern in patterns)
+                for pattern in patterns:
                     pattern_list.append(pattern)
+            print(pattern_list)
     else:
         sample_pattern = [{'patterns': ['([sS]ervers?)']}]
         with open(ymlFile, 'w') as pattern_file:
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 app_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-PATTERN_CONFIG_FILE = os.environ.get('PATTERN_CONFIG_FILE') or 'config/hostname_pattern.conf'
+PATTERN_CONFIG_FILE = os.environ.get('PATTERN_CONFIG_FILE') or 'config/hostname_pattern.yaml'
 INVENTORY_FILE = os.environ.get('INVENTORY_FILE') or 'inventory/hosts.ini'
 
 HTTP_PORT = os.environ.get('HTTP_PORT') or 5000
