0f7053c1d98ae4db76b9e7b2b4605da8503c403a
malahverdi <alahverdi_m@behsazan.net>
2022-04-04 11:05:17 +0430

    change agent

--- agent.sh
+++ agent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IP=$(hostname -I | cut -d " " -f1)
-HOSTNAME=$1
-URL=$2
+IP=$(ip a | grep $(ip link | awk -F: '$0 !~ "lo|vir|tun|docker|wl|br|^[^0-9]"{print $2;getline}') -A 5 | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -1)
+HOSTNAME=$(hostname)
+URL=$1
 
 curl -s -X POST -d "{\"IP\": \"$IP\", \"hostname\": \"$HOSTNAME\"}" -H "Content-Type: application/json" "$URL" | tee /tmp/tmway_status.txt
